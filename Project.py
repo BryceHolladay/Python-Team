7364fecfa4a9230eaa22920a954617e3db990b9e
ahbhaiwala <47577582+ahbhaiwala@users.noreply.github.com>
2021-04-24 15:51:09 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -9,11 +9,14 @@ import matplotlib.pyplot as plt
 
 # Function that takes in a csv file and outputs an average max peak, as well as saves a graph
 def find_max(file):
-    csv = pd.read_csv(file, header=3, nrows=36000)
-    fz = csv['Fz'].tolist() #creating a list of all Fz values
+    csv = pd.read_csv(file, header=[3], nrows=36000) #Header [3] to only include 4th row of column titles
+    cols = pd.read_csv(file,header=[3], nrows=0).columns.tolist()
     
-    print(csv)
-    print('Fz: ',fz)
+    fzR = csv['Fz'].tolist() #creating a list of all Fz values
+    fzL = csv['Fz.1'].tolist()
+    # print('FzR: ',fzR)
+    print('FzL: ',fzL)
+    
     return
 
 find_max('S4_T21.csv')
