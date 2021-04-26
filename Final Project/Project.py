438844102be0f2f25e62b92a3ff456ab0b3d5b46
Bryce Holladay <bryce.holladay@gmail.com>
2021-04-25 23:12:32 -0500

    Update Bryce 4/25 11pm

--- Final Project/Project.py
+++ Final Project/Project.py
@@ -80,6 +80,7 @@ def weight_cutoff_isolation(array, mass, mult=2):
 # Function that takes in a csv file and subject's mass (Kg) and outputs an average max peak, as well as saves a graph
 # noinspection PyUnresolvedReferences
 def find_max(file, subjectmass):
+    # Read in data from a csv file
     csv = pd.read_csv(file,
                       names=['fzL', 'fzR'],
                       usecols=[4, 13],
@@ -90,10 +91,10 @@ def find_max(file, subjectmass):
 
     fzR = csv['fzR'].tolist()  # creating a list of all Fz values on right foot
     fzL = csv['fzL'].tolist()  # create list of all Fz values on left leg
-
     fzR = np.negative(fzR)
     fzL = np.negative(fzL)
 
+    # Run plot and fft functions on our raw data
     plot(fzR, fzL, file.split('\\')[-1])
     fft_plot(fzR, fzL, file.split('\\')[-1])
 
@@ -124,48 +125,109 @@ def find_max(file, subjectmass):
     return FinalVals
 
 
+# Creates two bar graphs, one for right feet and one for left feet, comparing the two filtering methods
 def bar_graphs(vals):
     keys = vals.keys()
-    right = []
-    left = []
-    for k in keys:
-        #r_avg = (vals[k]['Top 10 Avg Method']['Right'][0] + vals[k]['BW Cutoff Method']['Right'][0]) / 2
-        #l_avg = (vals[k]['Top 10 Avg Method']['Left'][0] + vals[k]['BW Cutoff Method']['Left'][0]) / 2
-        r_avg = vals[k]['Top 10 Avg Method']['Right'][0]
-        l_avg = vals[k]['Top 10 Avg Method']['Left'][0]
-        right.append(r_avg)
-        left.append(l_avg)
 
+    # Right Foot
+
+    # Read mean values for each csv file
+    T10_R = []
+    BW_R = []
+    for k in keys:
+        a = vals[k]['Top 10 Avg Method']['Right'][0]
+        b = vals[k]['BW Cutoff Method']['Right'][0]
+        T10_R.append(a)
+        BW_R.append(b)
+
+    # Create bar graph
     barwidth = 0.25
-    data = [left, right]
+    data = [T10_R, BW_R]
     X = np.arange(len(keys))
     fig, ax = plt.subplots(figsize=(12,8))
+    plt.bar(X + 0.00, data[0], color='b', width=barwidth, label='Top 10 Avg Method')
+    plt.bar(X + 0.25, data[1], color='r', width=barwidth, label='BW Cutoff Method')
+
+    # Create data labels
     labels = []
     for n in range(len(data[0])):
         labels.append(data[0][n])
     for m in range(len(data[1])):
         labels.append(data[1][m])
     rects = ax.patches
+    i = 0
     for rect, label in zip(rects, labels):
-        plt.text(i.get_width()+0.2, i.get_y()+0.5,
-                 labels[i])
-    plt.bar(X + 0.00, data[0], color = 'b', width = barwidth, label='left')
-    plt.bar(X + 0.25, data[1], color = 'r', width = barwidth, label='right')
+        i += 1
+        if i <= 5:
+            Q = -0.075
+        else: Q = 0.075
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2 + Q, height + 5,
+                label.round(2), ha='center', va='bottom', fontweight='extra bold')
+
+    # Fill out other graph descriptors
     plt.xlabel("Trial")
     plt.ylabel('Reaction Force (N)')
     plt.xticks([r + barwidth for r in range(len(data[0]))], keys)
+    plt.legend(loc='lower right')
+    plt.title('Right Force Plate Measurements')
+
+    # Save bar graph locally
+    plt.savefig('graphs/bar_graph_right.png')
+    print('Right foot bar graph saved at .\\graphs\\bar_graph_right.png')
+
+
+    # Left Foot
+
+    # Read mean values for each csv file
+    T10_L = []
+    BW_L = []
+    for k in keys:
+        a = vals[k]['Top 10 Avg Method']['Left'][0]
+        b = vals[k]['BW Cutoff Method']['Left'][0]
+        T10_L.append(a)
+        BW_L.append(b)
+
+    # Create bar graph
+    barwidth = 0.25
+    data = [T10_L, BW_L]
+    X = np.arange(len(keys))
+    fig, ax = plt.subplots(figsize=(12, 8))
+    plt.bar(X + 0.00, data[0], color='b', width=barwidth, label='Top 10 Avg Method')
+    plt.bar(X + 0.25, data[1], color='r', width=barwidth, label='BW Cutoff Method')
+
+    # Create data labels
+    labels = []
+    for n in range(len(data[0])):
+        labels.append(data[0][n])
+    for m in range(len(data[1])):
+        labels.append(data[1][m])
+    rects = ax.patches
+    i = 0
     for rect, label in zip(rects, labels):
+        i += 1
+        if i <=5:
+            Q = -0.075
+        else: Q = 0.075
         height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width() / 2, height + 5,
+        ax.text(rect.get_x() + rect.get_width() / 2 + Q, height + 5,
                 label.round(2), ha='center', va='bottom', fontweight='extra bold')
 
+    # Fill out other graph descriptors
+    plt.xlabel("Trial")
+    plt.ylabel('Reaction Force (N)')
+    plt.xticks([r + barwidth for r in range(len(data[0]))], keys)
     plt.legend(loc='lower right')
-    plt.title('Left and Right Force Plate Measurements')
-    plt.savefig('graphs/bar_graph.png')
-    print('Bar graph saved at .\\graphs\\bar_graph.png')
+    plt.title('Left Force Plate Measurements')
+
+    # Save bar graph locally
+    plt.savefig('graphs/bar_graph_left.png')
+    print('Left foot bar graph saved at .\\graphs\\bar_graph_left.png')
 
 
+# Creates fast fourier transform plots
 def fft_plot(fzR, fzL, file_name):
+    # Create time values
     time = np.zeros(len(fzR)).tolist()
     t = 0
     for i in range(len(time)):
@@ -180,6 +242,7 @@ def fft_plot(fzR, fzL, file_name):
     fk = [k*df for k in range(N)]
     Gxx = [(2.0/N) * abs(Xk[i]) for i in range(N)]
 
+    # Complete FFT graph
     plt.figure()
     plt.plot(fk, Gxx)
     plt.xlabel('Frequency (Hz)')
@@ -187,6 +250,7 @@ def fft_plot(fzR, fzL, file_name):
     plt.title(file_name + ' Right Foot Fast Fourier Transform')
     plt.savefig('graphs/fft/' + file_name + '_FFT_RF.png')
 
+    # Enhanced viewing FFT graph
     plt.figure()
     plt.plot(fk, Gxx)
     plt.xlabel('Frequency (Hz)')
@@ -203,6 +267,7 @@ def fft_plot(fzR, fzL, file_name):
     fk = [k*df for k in range(N)]
     Gxx = [(2.0/N) * abs(Xk[i]) for i in range(N)]
 
+    # Complete FFT graph
     plt.figure()
     plt.plot(fk, Gxx)
     plt.xlabel('Frequency (Hz)')
@@ -210,6 +275,7 @@ def fft_plot(fzR, fzL, file_name):
     plt.title(file_name + ' Left Foot Fast Fourier Transform')
     plt.savefig('graphs/fft/' + file_name + '_FFT_LF.png')
 
+    # Enhanced viewing FFT graph
     plt.figure()
     plt.plot(fk, Gxx)
     plt.xlabel('Frequency (Hz)')
@@ -219,6 +285,7 @@ def fft_plot(fzR, fzL, file_name):
     plt.savefig('graphs/fft/' + file_name + '_FFT_LF_Zoom.png')
 
 
+# This function plots data for left and right feet
 def plot(fzR, fzL, file_name):
     time = np.zeros(len(fzR)).tolist()
     t = 0
@@ -236,6 +303,7 @@ def plot(fzR, fzL, file_name):
 
 
 def main():
+    # Reads all csv files in the current directory
     all_csv_files = []
     for root, dirs, files in os.walk('.', topdown=False):
         extensions = '.csv'
@@ -245,12 +313,14 @@ def main():
                 csv_file = os.path.join(root, f)
                 all_csv_files.append(csv_file)
 
+    # Informs user of csv files found
     print('There were', str(len(all_csv_files)), '.csv files found in directory', os.path.abspath('.'))
     for c in all_csv_files:
         print(c)
 
     print('\n\n===================\nBegin Data Analysis\n===================\n')
 
+    # Reads in weights from a text file
     weight_file = "./data/weights.txt"
     f = open(weight_file, "r")
     weights = {}
@@ -260,12 +330,14 @@ def main():
             weights[str(line_split[0])] = float(line_split[1])
         except: pass
 
+    # Performs max peaks analysis on each csv file found
     cleaned = {}
     for c in all_csv_files:
         csv_name = c.split('\\')[-1]
         print('--------------------')
         print(csv_name)
 
+        # Results of weight search for current file, prompting user for weight input if not found
         if csv_name in weights:
             wgt = weights[csv_name]
             print('Located runner weight at ' + weight_file + ': ' + str(wgt) + 'kg')
@@ -274,6 +346,7 @@ def main():
             text = 'Enter the weight of runner for file ' + str(csv_name) + ' in kg: '
             wgt = float(input(text))
 
+        # Prints data for user, storing results in a dictionary
         data_max = find_max(c, wgt)
         cleaned[csv_name] = data_max
         print('\nTop 10 Avg Method')
@@ -283,7 +356,29 @@ def main():
         print('Right Mean:', data_max['BW Cutoff Method']['Right'][0])
         print('Left Mean:', data_max['BW Cutoff Method']['Left'][0])
         print()
-        print('Plot saved at .\\graphs\\'+ csv_name +'_plot.png\n')
+        print('Plot saved at .\\graphs\\'+ csv_name +'_plot.png')
+        print('fft graphs saved to directory .\\graphs\\fft\n')
+
+    # Deletes old results file if it exists
+    if os.path.exists('results.txt'):
+        os.remove('results.txt')
+
+    # Writes results to a text file
+    f = open('results.txt', 'a')
+    for cl in cleaned.keys():
+        f.write(cl + '\n')
+        f.write('Top 10 Avg Method\n')
+        f.write('Right Mean: '+ str(cleaned[cl]['Top 10 Avg Method']['Right'][0]) + '\n')
+        f.write('Right Peaks: ' + str(cleaned[cl]['Top 10 Avg Method']['Right'][1]) + '\n')
+        f.write('Left Mean: ' + str(cleaned[cl]['Top 10 Avg Method']['Left'][0]) + '\n')
+        f.write('Left Peaks: ' + str(cleaned[cl]['Top 10 Avg Method']['Left'][1]) + '\n')
+        f.write('BW Cutoff Method\n')
+        f.write('Right Mean: ' + str(cleaned[cl]['BW Cutoff Method']['Right'][0]) + '\n')
+        f.write('Right Peaks: ' + str(cleaned[cl]['BW Cutoff Method']['Right'][1]) + '\n')
+        f.write('Left Mean: ' + str(cleaned[cl]['BW Cutoff Method']['Left'][0]) + '\n')
+        f.write('Left Peaks: ' + str(cleaned[cl]['BW Cutoff Method']['Left'][1]) + '\n')
+        f.write('\n')
+    f.close()
 
     print('====================\n')
     bar_graphs(cleaned)
